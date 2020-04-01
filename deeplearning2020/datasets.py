9e39d61093073ba63cabc1ec1b1c04448d0e2060
Martin Dreier <martin.dreier@web.de>
2020-04-01 17:17:36 +0200

    Allow caller to pass cache_dir parameter to Keras
    Setting the cache_dir parameter of function
    tensorflow.keras.utils.get_file allows for non-local file
    storage when running in Colab.

--- deeplearning2020/datasets.py
+++ deeplearning2020/datasets.py
@@ -44,7 +44,7 @@ class ImageWoof:
     image_count: int = 0
     list_ds: "_tf.data.Dataset" = None
 
-    def __init__(self, dataset: str) -> None:
+    def __init__(self, dataset: str, cache_dir: str = None) -> None:
         if dataset not in ["train", "val"]:
             raise ValueError("Dataset not found")
 
@@ -52,6 +52,7 @@ class ImageWoof:
             origin="https://s3.amazonaws.com/fast-ai-imageclas/imagewoof2-320.tgz",
             fname="imagewoof",
             untar=True,
+            cache_dir=cache_dir
         )
         self.data_dir = pathlib.Path(file_path + "2-320/" + dataset)
         print(self.data_dir)
